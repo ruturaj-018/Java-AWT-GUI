61397779be4daa8cf6266b74433b640adc88546f
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-10-10 23:09:56 +0530

    Update ComponentEventExample.java

--- Simple_Examples/ComponentEventExample.java
+++ Simple_Examples/ComponentEventExample.java
@@ -1,4 +1,30 @@
 package Simple_Examples;
+import java.awt.*;
+import java.awt.event.*;
+public class ComponentEventExample extends Frame implements ComponentListener {
 
-public class ComponentEventExample {
+    public ComponentEventExample() {
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public void componentResized(ComponentEvent e) {
+        System.out.println("Component resized");
+    }
+
+    public void componentMoved(ComponentEvent e) {
+        System.out.println("Component moved");
+    }
+
+    public void componentShown(ComponentEvent e) {
+        System.out.println("Component shown");
+    }
+
+    public void componentHidden(ComponentEvent e) {
+        System.out.println("Component hidden");
+    }
+
+    public static void main(String[] args) {
+        new ComponentEventExample();
+    }
 }
