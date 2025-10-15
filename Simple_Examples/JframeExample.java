1d1cc1d709b37560733ebf9354f1e96b91afe936
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-10-15 15:42:01 +0530

    Update JframeExample.java

--- Simple_Examples/JframeExample.java
+++ Simple_Examples/JframeExample.java
@@ -1,4 +1,36 @@
 package Simple_Examples;
+import javax.swing.*;
+public class Examp extends JFrame {
+    Examp() {
+        setTitle("Jpassward_Field");
+        setSize(500, 500);
+        setLayout(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-public class JframeExample {
+        JLabel la = new JLabel("Wel-Come");
+        la.setBounds(140, 60, 210, 80);
+        add(la);
+
+        JLabel l1 = new JLabel("Username :");
+        l1.setBounds(100, 150, 80, 30);
+        add(l1);
+
+        JLabel l2 = new JLabel("Passward :");
+        l2.setBounds(100, 200, 80, 30);
+        add(l2);
+
+        JTextField t1 = new JTextField();
+        t1.setBounds(190,150,90,30);
+        add(t1);
+
+        JPasswordField p1 = new JPasswordField(10);
+        p1.setBounds(190,200,90,30);
+        p1.setEchoChar('#');
+        add(p1);
+
+    }
+    public static void main(String[] args) {
+        new Examp();
+    }
 }
