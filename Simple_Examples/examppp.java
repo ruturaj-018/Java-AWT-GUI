7b3574282c78f6b6eb7630d01b4905cf12f46043
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-10-18 23:35:05 +0530

    Update examppp.java

--- Simple_Examples/examppp.java
+++ Simple_Examples/examppp.java
@@ -3,13 +3,16 @@ package Simple_Examples;
 import javax.swing.*;
 
 public class examppp {
-} add(l2);
+    add(l2);
 
-JTextField t1 = new JTextField();
+    JTextField t1 = new JTextField();
         t1.setBounds(190,150,90,30);
-add(t1);
 
-JPasswordField p1 = new JPasswordField(10);
+    add(t1);
+
+    JPasswordField p1 = new JPasswordField(10);
         p1.setBounds(190,200,90,30);
         p1.setEchoChar('#');
-add(p1);
+
+    add(p1);
+}
