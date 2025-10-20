b165f2ebe3b5180a31c11d870d7f1fe955c30921
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-10-20 23:00:10 +0530

    Update LoginJFrame.java

--- Simple_Examples/LoginJFrame.java
+++ Simple_Examples/LoginJFrame.java
@@ -31,6 +31,7 @@ public class LoginJFrame extends JFrame {
 
         }
         public static void main(String[] args) {
-            new LoginJFrame();
+        
+        new LoginJFrame();
         }
     }
