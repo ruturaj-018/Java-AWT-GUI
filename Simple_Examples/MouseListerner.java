18a4740289458aebe4fa5b87b7722baada61175e
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-10-22 22:52:46 +0530

    Update MouseListerner.java

--- Simple_Examples/MouseListerner.java
+++ Simple_Examples/MouseListerner.java
@@ -5,5 +5,39 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class MouseListerner extends Frame implements MouseListener {
+    MouseListerner() {
+        setTitle("Mouse_event");
+        setSize(500, 500);
+        setVisible(true);
+        addMouseListener(this);
+    }
+    @Override
+    public void mouseClicked (MouseEvent e){
+        setBackground(Color.BLUE);
 
+    }
+
+    @Override
+    public void mousePressed (MouseEvent e){
+        setBackground(Color.GREEN);
+    }
+
+    @Override
+    public void mouseReleased (MouseEvent e){
+        setBackground(Color.yellow);
+    }
+
+    @Override
+    public void mouseEntered (MouseEvent e){
+        setBackground(Color.RED);
+    }
+
+    @Override
+    public void mouseExited (MouseEvent e){
+        setBackground(Color.DARK_GRAY);
+    }
+
+    public static void main(String[] args) {
+        new Example();
+    }
 }
