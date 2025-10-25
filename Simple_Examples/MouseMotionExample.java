1b32fefd574d61d5d20d3b88c593ad6db7f904a1
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-10-25 23:44:33 +0530

    Update MouseMotionExample.java

--- Simple_Examples/MouseMotionExample.java
+++ Simple_Examples/MouseMotionExample.java
@@ -1,4 +1,32 @@
 package Simple_Examples;
 
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class MouseMotionExample {
+    {
+        Frame f1 = new Frame("Experiment No : 05");
+        f1.addMouseMotionListener(this);
+
+        f1.addWindowListener(w1);
+        f1.setLayout(null);
+        f1.setSize(500,500);
+        f1.setVisible(true);
+    }
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        System.out.println("Mouse dragged at: (" + e.getX() + ", " + e.getY() + ")");
+    }
+    WindowAdapter w1 = new WindowAdapter() {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            System.out.println("Mouse Dragged Event Completed Successfully.");
+            System.out.println("Window is closed. Goodbye!!!");
+            System.exit(0);
+        }
+    };
+
+    public static void main(String[] args) {
 }
