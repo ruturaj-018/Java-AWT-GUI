5c26df1d98fc8da091aec75d47664cc70707e854
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-10-29 22:58:58 +0530

    Update examp.java

--- Simple_Examples/examp.java
+++ Simple_Examples/examp.java
@@ -1,4 +1,12 @@
 package Simple_Examples;
 
 public class examp extend MouseListener{
+    textField = new TextField();
+        textField.setBounds(50, 50, 100, 30);
+        textField.addFocusListener(this);
+    add(textField);
+    setSize(200, 200);
+    setLayout(null);
+    setVisible(true);
+}
 }
