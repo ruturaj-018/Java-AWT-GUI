69fff25bf26a2ad78ca343a463bd50789aa0cbf2
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-10-31 10:43:22 +0530

    Update examp.java

--- Simple_Examples/examp.java
+++ Simple_Examples/examp.java
@@ -1,7 +1,7 @@
 package Simple_Examples;
 
 public class examp {
-    textField = new TextField();
+    textField = new TextField()
         textField.setBounds(50, 50, 100, 30);
         textField.addFocusListener(this);
     add(textField);
