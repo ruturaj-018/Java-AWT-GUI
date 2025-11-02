e8bfdb5141bcddf4adab74b84923e1811eaf232b
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-11-02 23:31:06 +0530

    Update FocusEventExample.java

--- Simple_Examples/FocusEventExample.java
+++ Simple_Examples/FocusEventExample.java
@@ -4,11 +4,13 @@ import java.awt.*;
 import java.awt.event.FocusListener;
 
 public class FocusEventExample extends Frame implements FocusListener {
-    textField = new TextField();
+    public FocusEventExample() {
+        textField = new TextField();
         textField.setBounds(50, 50, 100, 30);
         textField.addFocusListener(this);
-    add(textField);
-    setSize(200, 200);
-    setLayout(null);
-    setVisible(true)
+        add(textField);
+        setSize(200, 200);
+        setLayout(null);
+        setVisible(true);
+    }
 }
