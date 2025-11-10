a68c99bac5947d6a28524513d0f7930239787c7a
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-11-10 23:46:13 +0530

    Update InputEventExample.java

--- Simple_Examples/InputEventExample.java
+++ Simple_Examples/InputEventExample.java
@@ -1,4 +1,11 @@
 package Simple_Examples;
 
 public class InputEventExample {
+    public InputEventExample() {
+        addKeyListener(this);
+        addMouseListener(this);
+        setSize(200, 200);
+        setLayout(null);
+        setVisible(true);
+    }
 }
