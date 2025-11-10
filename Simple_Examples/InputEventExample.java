76e821c09c38ab5f4e41accaa0d1fda5ca75ebf0
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-11-10 23:47:32 +0530

    Update InputEventExample.java

--- Simple_Examples/InputEventExample.java
+++ Simple_Examples/InputEventExample.java
@@ -1,5 +1,8 @@
 package Simple_Examples;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
 public class InputEventExample {
     public InputEventExample() {
         addKeyListener(this);
@@ -8,4 +11,21 @@ public class InputEventExample {
         setLayout(null);
         setVisible(true);
     }
+
+    public void keyPressed(KeyEvent e) {
+        System.out.println("Key pressed: " + e.getKeyChar());
+    }
+
+    public void keyReleased(KeyEvent e) {
+        System.out.println("Key released: " + e.getKeyChar());
+    }
+
+    public void keyTyped(KeyEvent e) {
+        System.out.println("Key typed: " + e.getKeyChar());
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("Mouse clicked at: " + e.getX() + ", " + e.getY());
+    }
+
 }
