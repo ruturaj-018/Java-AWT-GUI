cadc1fa634dcb154269159b70abd4fc62524daaa
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-11-14 19:27:15 +0530

    Update ItemEventExample.java

--- Simple_Examples/ItemEventExample.java
+++ Simple_Examples/ItemEventExample.java
@@ -1,4 +1,25 @@
 package Simple_Examples;
+import java.awt.*;
+import java.awt.event.*;
 
-public class ItemEventExample {
+public class ItemEventExample extends Frame implements ItemListener {
+    Checkbox checkbox;
+
+    public ItemEventExample() {
+        checkbox = new Checkbox("Check me");
+        checkbox.setBounds(50, 50, 100, 30);
+        checkbox.addItemListener(this);
+        add(checkbox);
+        setSize(200, 200);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public void itemStateChanged(ItemEvent e) {
+        System.out.println("Checkbox state changed: " + (e.getStateChange() == ItemEvent.SELECTED ? "Selected" : "Deselected"));
+    }
+
+    public static void main(String[] args) {
+        new ItemEventExample();
+    }
 }
