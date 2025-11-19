519b986ee63534585a9a40c34fe1118b07a1bdf0
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-11-19 09:37:13 +0530

    Update KeyAdpterExample.java

--- Simple_Examples/KeyAdpterExample.java
+++ Simple_Examples/KeyAdpterExample.java
@@ -1,7 +1,19 @@
 package Simple_Examples;
 
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class KeyAdpterExample {
     public static void main(String[] args) {
-       
+        Frame frame = new Frame("Key Adapter Example");
+        frame.setSize(300, 200);
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                System.out.println("Key Pressed: " + e.getKeyChar());
+            }
+        });
+        frame.setVisible(true);
     }
 }
