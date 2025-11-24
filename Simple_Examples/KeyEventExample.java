a29894b11d1deab0a6e8607e9f9cbeb11721b5fe
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-11-24 22:10:38 +0530

    Update KeyEventExample.java

--- Simple_Examples/KeyEventExample.java
+++ Simple_Examples/KeyEventExample.java
@@ -1,2 +1,30 @@
 package Simple_Examples;
+import java.awt.*;
+import java.awt.event.*;
+
+public class KeyEventExample extends Frame implements KeyListener {
+
+    public KeyEventExample() {
+        addKeyListener(this);
+        setSize(200, 200);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public void keyPressed(KeyEvent e) {
+        System.out.println("Key pressed: " + e.getKeyChar());
+    }
+
+    public void keyReleased(KeyEvent e) {
+        System.out.println("Key released: " + e.getKeyChar());
+    }
+
+    public void keyTyped(KeyEvent e) {
+        System.out.println("Key typed: " + e.getKeyChar());
+    }
+
+    public static void main(String[] args) {
+        new KeyEventExample();
+    }
+}
 
