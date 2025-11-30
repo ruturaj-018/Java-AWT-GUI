4ffa4601b8121864a06e3139a2899c4b79bad3e6
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-11-30 23:17:47 +0530

    Update MouseAdapterExample.java

--- Simple_Examples/MouseAdapterExample.java
+++ Simple_Examples/MouseAdapterExample.java
@@ -1,5 +1,26 @@
 package Simple_Examples;
 
-public class MouseAdapterExample 
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MouseAdapterExample
+import java.awt.Frame;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MouseAdapterExample {
+    public static void main(String[] args) {
+        Frame frame = new Frame("Mouse Adapter Example");
+        frame.setSize(300, 200);
+        frame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("Mouse Clicked at: (" + e.getX() + ", " + e.getY() + ")");
+            }
+        });
+        frame.setVisible(true);
+    }
+}
     }
 }
