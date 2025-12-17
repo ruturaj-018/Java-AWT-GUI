fac5205069e3677e309bad70bea357f8c2b5d339
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-12-17 19:12:16 +0530

    Update MouseEventExample.java

--- Simple_Examples/MouseEventExample.java
+++ Simple_Examples/MouseEventExample.java
@@ -1,5 +1,10 @@
 package Simple_Examples;
 
 public class MouseEventExample {
-   
+    public MouseEventExample() {
+        addMouseListener(this);
+        setSize(200, 200);
+        setLayout(null);
+        setVisible(true);
+    }
 }
