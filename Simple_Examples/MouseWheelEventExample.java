6d16047e7990ea3859e5cde90f29e9ddddd77e9f
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-12-25 21:07:57 +0530

    Update MouseWheelEventExample.java

--- Simple_Examples/MouseWheelEventExample.java
+++ Simple_Examples/MouseWheelEventExample.java
@@ -2,4 +2,12 @@ package Simple_Examples;
 import java.awt.*;
 import java.awt.event.*;
 public class MouseWheelEventExample extends Frame implements MouseWheelListener {
+
+    public MouseWheelEventExample() {
+        addMouseWheelListener(this);
+        setSize(200, 200);
+        setLayout(null);
+        setVisible(true);
+    }
+    
 }
