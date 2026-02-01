0d00df3dda7219ad0390590b32acda83ec71322b
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2026-02-01 10:05:17 +0530

    Add two text fields to window example
    Introduced two TextField components to the window example, with the second field set as non-editable.

--- Simple_Examples/window.java
+++ Simple_Examples/window.java
@@ -7,5 +7,7 @@ public class TextCopieEvent {
         Frame frame = new Frame("TextEvent Example");
         frame.setSize(400, 200);
         frame.setLayout(new GridLayout(3, 1, 10, 10));
-
         // Create text fields
+        TextField textField1 = new TextField();
+        TextField textField2 = new TextField();
+        textField2.setEditable(false); // Make the second text field non-editable
