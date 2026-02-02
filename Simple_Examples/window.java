610881cf096232b49baeb5cf63c10b620af53a19
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2026-02-02 23:37:11 +0530

    Update window.java

--- Simple_Examples/window.java
+++ Simple_Examples/window.java
@@ -7,7 +7,14 @@ public class TextCopieEvent {
         Frame frame = new Frame("TextEvent Example");
         frame.setSize(400, 200);
         frame.setLayout(new GridLayout(3, 1, 10, 10));
+        
         // Create text fields
         TextField textField1 = new TextField();
         TextField textField2 = new TextField();
         textField2.setEditable(false); // Make the second text field non-editable
+
+        // Add components to the frame
+        frame.add(new Label("Enter text in the first field:"));
+        frame.add(textField1);
+        frame.add(new Label("Copied text will appear here:"));
+        frame.add(textField2);
