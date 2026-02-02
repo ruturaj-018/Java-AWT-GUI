fa56ddbcfbdd566c881d45c0df54e14019d7a3c8
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2026-02-02 23:39:37 +0530

    Update window.java

--- Simple_Examples/window.java
+++ Simple_Examples/window.java
@@ -1,13 +1,13 @@
 import java.awt.*;
 import java.awt.event.*;
 
-public class TextCopieEvent {
+public class TextCopyEvent {
     public static void main(String[] args) {
         // Create the main frame
         Frame frame = new Frame("TextEvent Example");
         frame.setSize(400, 200);
         frame.setLayout(new GridLayout(3, 1, 10, 10));
-        
+
         // Create text fields
         TextField textField1 = new TextField();
         TextField textField2 = new TextField();
