40bcabcf136e467f2bd5935be5fcf8b2579bdb85
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2026-02-15 19:28:42 +0530

    Update example.java

--- Simple_Examples/example.java
+++ Simple_Examples/example.java
@@ -3,4 +3,5 @@ import java.awt.event.*;
 
 public class TextEventExample extends Frame implements TextListener {
     TextField textField;
-    
+    public TextEventExample() {
+        textField = new TextField();
