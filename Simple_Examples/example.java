2c3dbfa425a0321479fee43038afe05eb7425ae0
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2026-02-16 17:21:56 +0530

    Update example.java

--- Simple_Examples/example.java
+++ Simple_Examples/example.java
@@ -8,3 +8,6 @@ public class TextEventExample extends Frame implements TextListener {
         textField.setBounds(50, 50, 100, 30);
         textField.addTextListener(this);
         add(textField);
+        setSize(200, 200);
+        setLayout(null);
+        setVisible(true);
