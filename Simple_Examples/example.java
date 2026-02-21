687699cea63c84164caee71545a89a0ff4eada6f
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2026-02-21 20:45:25 +0530

    Update example.java

--- Simple_Examples/example.java
+++ Simple_Examples/example.java
@@ -18,6 +18,6 @@ public class TextEvent extends Frame implements TextListener {
     }
 
     public static void main(String[] args) {
-        new TextEventExaple();
+        new TextEveExaple();
     }
 }
