e63a9641a145d8e6013757092acd4122cf9ad0dc
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2026-03-03 02:17:48 +0900

    Update example.java

--- Simple_Examples/example.java
+++ Simple_Examples/example.java
@@ -1,7 +1,14 @@
 package Simple_Examples;
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class example {
     public static void main(String[] args) {
-        
+        Frame f1 = new Frame("Experiment No : 05");
+        WindowAdapter w1 = new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
     }
 }
