2527ea1a8eed192da9f9238300dfd2e9095a1050
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2026-03-11 22:44:14 +0530

    Update examl.java

--- Simple_Examples/examl.java
+++ Simple_Examples/examl.java
@@ -1,7 +1,9 @@
 package Simple_Examples;
 
+import java.awt.*;
+
 public class examl  {
     public static void main(String[] args) {
-        
+        Frame frame = new Frame("Window Adapter Example");
     }
 }
