0df2f6bdf1e0c29b2bbd4f1cfb154aff4bf9e51c
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2026-03-28 23:57:56 +0530

    Update examl.java

--- Simple_Examples/examl.java
+++ Simple_Examples/examl.java
@@ -4,9 +4,9 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class  windowsadapater {
+public class  windowadapater {
     public static void main(String[] args) {
-        Frame frame = new Frame("Window Adapter Example ");
+        Frame frame = new Frame("Window Adapter Example");
         frame.setSize(300, 200);
         frame.addWindowListener(new WindowAdapter() {
 
